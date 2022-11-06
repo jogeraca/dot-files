9574ed426c3b862e086fa9179a64828430beca0b
Yoser <jogeraca@gmail.com>
2022-11-06 13:51:36 -0500

    added keymaps to git commands

--- nvim_lua/after/plugin/vim-fugitive.lua
+++ nvim_lua/after/plugin/vim-fugitive.lua
@@ -6,7 +6,7 @@ map("n", "<c-p>", ":GitFiles<cr>", noremap_opts)
 map("n", "<leader>gs", "<cmd>Git<cr>", noremap_opts)
 map("n", "<leader>gw", "<cmd>Gwrite<cr>", noremap_opts)
 map("n", "<leader>gc", "<cmd>Gcommit<cr>", noremap_opts)
-map("n", "<leader>gsh", "<cmd>Gpush<cr>", noremap_opts)
+map("n", "<leader>gsh", "<cmd>Git push<cr>", noremap_opts)
 map("n", "<leader>gll", "<cmd>Gpull<cr>", noremap_opts)
 map("n", "<leader>Gw", "<cmd>Gwq<cr>", noremap_opts)
 map("n", "<leader>Gb", "<cmd>Gwq<cr>", noremap_opts)
@@ -14,4 +14,9 @@ map("n", "<leader>Gvh", "<cmd>Gvdiffsplit<cr>", noremap_opts)
 map("n", "<leader>Ghh", "<cmd>Gdiffsplit<cr>", noremap_opts)
 
 map("n", "<leader>gd", ":silent !tmux popup -K -w '90\\%' -h '90\\%' -R 'git diff'<cr>", noremap_opts)
+
+-- resolv conflicts
+map("n", "<S-l>", ":diffget LO <cr>", noremap_opts)
+map("n", "<S-r>", ":diffget RE<cr>", noremap_opts)
+map("n", "<S-b>", ":diffget BA<cr>", noremap_opts)
 --vim.keymap.set("n", "<leader>gs", ":silent !tmux popup -K -w '90\\%' -h '90\\%' -R 'git status'<cr>")
